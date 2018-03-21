42753580cb241e6b405236c23b679e833212a87d
Ubuntu <jiwang@sagemark.y2hrku3smsoejfp5gn0zfzwbzg.gx.internal.cloudapp.net>
2018-03-21 07:03:12 +0000

    Add hard-coded token

--- examples/sagemaker/cmle_client.py
+++ examples/sagemaker/cmle_client.py
@@ -9,7 +9,8 @@ class UserTasks(TaskSet):
         fname = os.getcwd() + "/iris_inference_data.json"
         with open(fname, 'rb') as f:
             payload = f.read()
-        token = subprocess.check_output(["gcloud", "auth", "print-access-token"])
+#        token = subprocess.check_output(["gcloud", "auth", "print-access-token"])
+        token = "ya29.GluFBZozHsdbX-KI2NmcbvEmhSaayQ-5aEtaqMoB0SaEyqZQq0V7UCYpkiYEKAzt2cL83YX5unTmUaLr5f0qUdZ0N6Wj5EuPEL54bsIP1XTsBlfpSS-L88QFWIUM\n"
         self.client.headers['Authorization'] = 'Bearer ' + token[:-1]
         self.client.post("", payload, auth="something")
             
