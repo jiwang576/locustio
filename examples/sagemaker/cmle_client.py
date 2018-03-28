4bd441b73c0bf076884c575c28d08c65a40b308e
Ubuntu <jiwang@sagemark.y2hrku3smsoejfp5gn0zfzwbzg.gx.internal.cloudapp.net>
2018-03-28 22:50:09 +0000

    Use CMLE hard-coded token

--- examples/sagemaker/cmle_client.py
+++ examples/sagemaker/cmle_client.py
@@ -9,7 +9,8 @@ class UserTasks(TaskSet):
         fname = os.getcwd() + "/criteo_inference_data.json"
         with open(fname, 'rb') as f:
             payload = f.read()
-        token = subprocess.check_output(["gcloud", "auth", "print-access-token"])
+        #token = subprocess.check_output(["gcloud", "auth", "print-access-token"])
+        token = "ya29.GlyMBYy5sd_ovNkSTZSOntQ8Ltl5vC1I5xasghuGYSryJaY-Cex1U2W9ngI6HopcmuLmYd1UXONRJeIb0nDQv3YLU9ZFyxajzh-yUVx-zzgSKG016NIVNwolEkYC1w\n"
         self.client.headers['Authorization'] = 'Bearer ' + token[:-1]
         self.client.post("", payload, auth="something")
             
@@ -17,6 +18,6 @@ class WebsiteUser(HttpLocust):
     """
     Locust user class that does requests to the locust web server running on localhost
     """
-    min_wait = 1000
-    max_wait = 1000
+    min_wait = 200
+    max_wait = 200
     task_set = UserTasks
